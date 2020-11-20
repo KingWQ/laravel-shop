95392508f29eed1e7e6c3f02023fbfa7dcb8bbce
August <august1122wq@gmail.com>
2020-11-20 17:24:00 +0800

    调整horizon只能后台访问

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -22,7 +22,9 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         // Horizon::routeMailNotificationsTo('example@example.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
 
-
+        Horizon::auth(function () {
+            return \Admin::user()->can('horizon');
+        });
         // Horizon::night();
     }
 
